fd6733a5cabe4a41b4ea99220c0322d57717f363
ziyaudheenms <msharafudeen193@gmail.com>
2026-02-07 18:19:27 +0530

    implemented the cache

--- fileBox/Backend/admin.py
+++ fileBox/Backend/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import ClerkUserProfile , FileModel , FileFolderModel
+from .models import ClerkUserProfile  , FileFolderModel , ClerkUserStorage
 # Register your models here.
 
 admin.site.register(ClerkUserProfile)
-admin.site.register(FileModel)
 admin.site.register(FileFolderModel)
+admin.site.register(ClerkUserStorage)
--- fileBox/fileBox/settings.py
+++ fileBox/fileBox/settings.py
@@ -92,6 +92,19 @@ CHANNEL_LAYERS = {
     },
 }
 
+# settings.py example for a specific versioned cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'TIMEOUT' : None,
+    }
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
